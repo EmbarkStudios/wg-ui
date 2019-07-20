d1d69290fe8125142e65503b85ab7b03ae308311
Daniel Lundin <dln@eintr.org>
2019-07-20 12:34:14 +0200

    Add some text to the about page

--- ui/src/About.svelte
+++ ui/src/About.svelte
@@ -1,3 +1,19 @@
 <h1>About</h1>
 
-Wireguard is nice.
+<p>
+Wireguard UI is an <a href="https://embark-studios.com/">Embark Studios</a> Open Source project.
+</p>
+
+<p>
+For contributions and feedback, please see the
+<a href="https://github.com/EmbarkStudios/wireguard-ui">GitHub project</a>.
+</p>
+
+<h2>License</h2>
+<p>
+Wireguard UI is licensed under <a href="https://www.apache.org/licenses/LICENSE-2.0">Apache License, Version 2.0</a>
+</p>
+
+<p>Copyright &copy; 2019, Embark Studios AB</p>
+
+
